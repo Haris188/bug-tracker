ac1274be736d743ed65f53f0d2aed080cf75e252
haris188 <harisahmad.188@gmail.com>
2020-10-10 19:33:07 -0400

    session right

--- src/middlewares/installCors.ts
+++ src/middlewares/installCors.ts
@@ -2,6 +2,13 @@
 import * as cors from 'cors'
 
 export default (app:any)=>{
+    app.use((req,res)=>{
+        res.header("Access-Control-Allow-Origin", "https://lfatrack.herokuapp.com");
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+    })
+
     app.use(cors({
         credentials:true, 
         origin:true
