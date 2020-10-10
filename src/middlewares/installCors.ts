15fe5971f1bc8a6f3712e095eccdee6edff8d083
haris188 <harisahmad.188@gmail.com>
2020-10-10 19:36:29 -0400

    session right

--- src/middlewares/installCors.ts
+++ src/middlewares/installCors.ts
@@ -3,7 +3,7 @@ import * as cors from 'cors'
 
 export default (app:any)=>{
     app.use((req,res)=>{
-        res.header("Access-Control-Allow-Origin", "https://lfatrack.herokuapp.com");
+        res.header("Access-Control-Allow-Origin", "https://lfa.netlify.app/");
     //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
