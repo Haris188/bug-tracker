41e71cbc19dfc943d0fb595e7b9693e5a10f21be
haris188 <harisahmad.188@gmail.com>
2020-10-10 19:46:02 -0400

    session right

--- src/middlewares/installCors.ts
+++ src/middlewares/installCors.ts
@@ -3,10 +3,9 @@ import * as cors from 'cors'
 
 export default (app:any)=>{
     app.use((req,res, next)=>{
-        res.header("Access-Control-Allow-Origin", "https://lfa.netlify.app/");
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Origin", "*");
         res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next()
     })
 
